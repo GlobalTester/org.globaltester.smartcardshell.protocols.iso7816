65cb683a3f2ca840b68d6fd81fda663cb0016ecd
Alexander May <alexander.may@hjp-consulting.com>
2012-02-10 14:59:00 +0000

    Initial version without functionality
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.smartcardshell.protocols.iso7816/trunk@483 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/smartcardshell/protocols/ios7816/ProtocolProvider.java
@@ -0,0 +1,17 @@
+package org.globaltester.smartcardshell.protocols.ios7816;
+
+import java.util.List;
+
+import org.globaltester.smartcardshell.protocols.AbstractScshProtocolProvider;
+import org.globaltester.smartcardshell.protocols.ScshCommand;
+
+
+public class ProtocolProvider extends AbstractScshProtocolProvider {
+
+	@Override
+	public void addCommands(List<ScshCommand> commandList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
